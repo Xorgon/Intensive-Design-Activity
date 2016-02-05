1c0fa43700381344551716cbb1827f53cf148fb3
Xorgon <norman.reggox@gmail.com>
2016-02-05 17:42:23 +0000

    Added graph generation script

---
+++ generateGraph.m
@@ -0,0 +1,28 @@
+lift_max = 1.08;
+lift_min = 1.01;
+lift_steps = 20;
+
+drag_max = 0.20;
+drag_min = 0.14;
+drag_steps = 20;
+
+lift = zeros(1, lift_steps);
+drag = zeros(1, drag_steps);
+times = [];
+
+for i = 1:(lift_steps + 1)
+    for j = 1:(drag_steps + 1)
+        lifti = lift_min + (i - 1)*(lift_max - lift_min)/lift_steps;
+        dragj = drag_min + (j - 1)*(drag_max - drag_min)/drag_steps;
+        lift(i) = lifti;
+        drag(j) = dragj;
+        times(j,i) = simulator([lifti, dragj]);
+    end
+end
+
+delete(findall(0))
+bdclose('all')
+
+liftlabel('Coefficient of Lift')
+draglabel('Coefficient of Drag')
+surf(lift, drag, times)
